31015185afaa10572d04eaf0dfe22402cbe1a847
deking254 <60552782+deking254@users.noreply.github.com>
2024-04-27 13:35:17 +0300

    Delete 0x00-Webpack directory

--- 0x00-Webpack/task_3/modules/header/header.js
+++
@@ -1,4 +0,0 @@
-import * as css from './header.css';
-const jQuery = require("jquery");
-jQuery("html").append("<header><div id='logo'></div>\n<h1>Holberton Dashboard</h1></header>");
-console.log("Init header");
